7ee4f7fc57b80b3f9f6eb354567ffd72e5849ba7
Gabriel Farias <gfariasr@users.noreply.github.com>
2017-01-06 21:34:19 +0000

    Automatically generated documentation

--- search/files_11.js
+++ search/files_11.js
@@ -7,6 +7,7 @@ var searchData=
   ['severity_5flevel_2ecpp',['severity_level.cpp',['../severity__level_8cpp.html',1,'']]],
   ['severity_5flevel_2ehpp',['severity_level.hpp',['../severity__level_8hpp.html',1,'']]],
   ['short_5fstring_5ffield_2ehpp',['short_string_field.hpp',['../short__string__field_8hpp.html',1,'']]],
+  ['show_5fdevice_2ecpp',['show_device.cpp',['../show__device_8cpp.html',1,'']]],
   ['static_5fdigits_2ehpp',['static_digits.hpp',['../static__digits_8hpp.html',1,'']]],
   ['stock_5fdirectory_5fmessage_2ecpp',['stock_directory_message.cpp',['../stock__directory__message_8cpp.html',1,'']]],
   ['stock_5fdirectory_5fmessage_2ehpp',['stock_directory_message.hpp',['../stock__directory__message_8hpp.html',1,'']]],
@@ -14,6 +15,7 @@ var searchData=
   ['stock_5ftrading_5faction_5fmessage_2ecpp',['stock_trading_action_message.cpp',['../stock__trading__action__message_8cpp.html',1,'']]],
   ['stock_5ftrading_5faction_5fmessage_2ehpp',['stock_trading_action_message.hpp',['../stock__trading__action__message_8hpp.html',1,'']]],
   ['strtonum_2ehpp',['strtonum.hpp',['../strtonum_8hpp.html',1,'']]],
+  ['style_5fguide_2emd',['style_guide.md',['../style__guide_8md.html',1,'']]],
   ['system_5fevent_5fmessage_2ecpp',['system_event_message.cpp',['../system__event__message_8cpp.html',1,'']]],
   ['system_5fevent_5fmessage_2ehpp',['system_event_message.hpp',['../system__event__message_8hpp.html',1,'']]]
 ];
